39949bb2e29e1d84901f1ebb03f78465d9ea8241
elinorkochanski <kochanse@mail.gvsu.edu>
2023-12-01 14:27:23 -0500

    new functions

--- math_functions.py
+++ math_functions.py
@@ -6,6 +6,10 @@ def multiply_numbers(a, b):
     return a * b
 def divide_numbers(a, b):
     return a / b
+def square_number(a):
+    return a * a
+def linear_func(a, b, x):
+    return (a * x) + b
 if __name__ == "__main__":
     print("Adding:", add_numbers(2,4))
     print("Subtracting:", subtract_numbers(9,2))
